d78ee4225373d4995f2f0bdba3214de038bf595b
Sonali kumari <68367399+sonali6062@users.noreply.github.com>
2025-09-08 19:42:34 +0530

    Create 1317. Convert Integer to the Sum of Two No-Zero Integers.cpp

---
+++ leetcode daily problems/September/1317. Convert Integer to the Sum of Two No-Zero Integers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool hasZero(int x) {
+        while (x > 0) {
+            if (x % 10 == 0)
+                return true;
+            x /= 10;
+        }
+        return false;
+    }
+    
+    vector<int> getNoZeroIntegers(int n) {
+        for (int i = 1; i < n; ++i) {
+            int j = n - i;
+            if (!hasZero(i) && !hasZero(j)) {
+                return {i, j};
+            }
+        }
+        return {}; // Just in case, though problem guarantees a solution.
+    }
+};
