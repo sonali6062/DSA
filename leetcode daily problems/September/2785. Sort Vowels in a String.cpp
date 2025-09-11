957c471628e561ffc16e2501e13bf00ea0be51b7
Sonali kumari <68367399+sonali6062@users.noreply.github.com>
2025-09-11 10:14:17 +0530

    Create 2785. Sort Vowels in a String.cpp

---
+++ leetcode daily problems/September/2785. Sort Vowels in a String.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        int n=s.size();
+        vector<char>ch;
+        for(char c:s){
+            if(isVowel(c)){
+                ch.push_back(c);
+
+            }
+        }
+        sort(ch.begin(),ch.end());
+        int v_index=0;
+        for(char &c:s){
+            if(isVowel(c)){
+                c=ch[v_index++];
+            }
+        }
+
+        return s;
+    }
+    bool isVowel(char c){
+        switch(c){
+            case 'A':case 'E':case 'I':case 'O':case 'U':
+            case 'a':case 'e':case 'i':case 'o':case 'u':
+            return true;
+            default:
+            return false;
+
+        }
+    }
+};
